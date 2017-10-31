8445a9eb2f1c5a56d5726a4b6e65de7793fa2ed1
Kamil Żmijowski <kamz8@wp.pl>
2017-10-31 18:59:30 +0100

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+Procfile  vendor/bin/heroku-php-apache2 public/
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register Procfile routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "Procfile" middleware group. Now create something great!
 |
 */
 
