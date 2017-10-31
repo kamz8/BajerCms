0007535f5782190d6925a2d50724b5cba197f0c4
Kamil Żmijowski <kamz8@wp.pl>
2017-10-31 22:34:24 +0100

    sbAdmin template

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SB Admin - Start Bootstrap Template</title>
+  <!-- Bootstrap core CSS-->
+  <link href="{{asset('css//bootstrap.min.css')}}" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="{{asset('css/sb-admin.css')}}" rel="stylesheet">
+</head>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
+<div class="content-wrapper">
+  <div class="container-fluid">
+    @yield('content')
+  </div>
+  <!-- /.container-fluid-->
+  <!-- /.content-wrapper-->
+  <footer class="sticky-footer">
+    <div class="container">
+      <div class="text-center">
+        <small>Copyright © Your Website 2017</small>
+      </div>
+    </div>
+  </footer>
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fa fa-angle-up"></i>
+  </a>
+  <!-- Logout Modal-->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="{{asset('js/jquery.easing.min.js')}}"></script>
+  <!-- Page level plugin JavaScript-->
+  <script src="{{asset('js/Chart.min.js')}}"></script>
+  <script src="{{asset('js/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('js/dataTables.bootstrap4.js')}}"></script>
+  <!-- Custom scripts for all pages-->
+  <script src="{{asset('js/sb-admin.min.js')}}"></script>
+  <!-- Custom scripts for this page-->
+  <script src="{{asset('js/sb-admin-datatables.min.js')}}"></script>
+  <script src="{{asset('js/sb-admin-charts.min.js')}}"></script>
+
+</div>
+<script></script>
+</body>
+
+</html>
+
