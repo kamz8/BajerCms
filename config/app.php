f5ba9dd64276cc13418e1bb9e09fb09c78b640d1
Kamil <kamz8@wp.pl>
2017-11-12 16:31:00 +0100

    rm laravel ide helper

--- config/app.php
+++ config/app.php
@@ -175,8 +175,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        App\Providers\RouteServiceProvider::class
+
     ],
 
     /*
