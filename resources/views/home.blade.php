e823b43b887d97f816ee31efa753519db998173c
Kamil <kamz8@wp.pl>
2018-04-10 23:42:19 +0200

    Merge branch 'dev' of https://github.com/kamz8/BajerCms into dev
    # Conflicts:
    #	.env
    #	app/User.php
    #	composer.lock
    #	config/app.php
    #	database/migrations/2014_10_12_000000_create_users_table.php
    #	public/js/app.js
    #	resources/assets/js/bootstrap.js
    #	routes/api.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -143,7 +143,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6"></div>
-                <div class="col-md-6"></div>
+                <div class="col-md  -6"></div>
             </div>
         </div>
     </section>
