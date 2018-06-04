a6f1b5405936ee3d4bfa22debbed753783cdeeb8
Kamil Żmijowski <kamz8@wp.pl>
2018-06-04 13:20:46 +0200

    Update http-comon.js

--- resources/assets/js/http-comon.js
+++ resources/assets/js/http-comon.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'http://http://bajercms.herokuapp.com/api',
+  baseURL: 'http://bajercms.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept':'application/json'
