c1c044a12e0cc41410e6de8181968907aaa3255e
Kamil Żmijowski <kamz8@wp.pl>
2018-06-04 13:14:49 +0200

    Update http-comon.js

--- resources/assets/js/http-comon.js
+++ resources/assets/js/http-comon.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: process.env.baseURL || 'http://localhost:8000/api',
+  baseURL: process.env.APP_API_URL || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept':'application/json'
