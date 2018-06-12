91c5ccb3d7bebc68205c04875eb6d755c02bb740
kamz8 <kamz8@wp.pl>
2018-06-12 17:27:41 +0200

    staging http setings

--- resources/assets/js/http-comon.js
+++ resources/assets/js/http-comon.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: process.env.APP_API_URL || 'http://localhost:8000/api',
-  // baseURL: 'http://bajercms.herokuapp.com/api',
+  // baseURL: process.env.APP_API_URL || 'http://localhost:8000/api',
+  baseURL: 'http://bajercms.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept':'application/json'
