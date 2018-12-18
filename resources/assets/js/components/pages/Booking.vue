1e9680a8b0521467716aa7a2dc7d300ae59e44bf
Staszek Markowski <stasiomimi@hotmail.com>
2018-12-18 23:04:51 +0100

    Add files via upload
    Strona wynajmy - uzupełnienia tekstów i FAQ

--- resources/assets/js/components/pages/Booking.vue
+++ resources/assets/js/components/pages/Booking.vue
@@ -8,11 +8,14 @@
                             <h2 class="text-uppercase pb-2">Wynajmij salę na własne potrzeby</h2>
                             <div class="mr-1 text-justify">
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare dictum dolor vitae viverra. Donec condimentum mattis tellus id vehicula. Aenean euismod risus id vestibulum consequat.
-                                    Mauris rhoncus, eros vel venenatis condimentum, diam erat dictum ipsum, nec tristique ligula tellus at lacus. Nullam ac sodales eros. Aliquam quis lacinia ipsum, eget mollis magna. Morbi odio ligula,
-                                    vehicula dignissim tempor a, hendrerit vitae dolor. Proin ullamcorper metus quis magna ultricies, non tincidunt dui luctus. Vivamus sed mi porttitor, euismod sapien id, fermentum nisi.</p>
-                                <p>Proin vehicula non sapien a dapibus. Etiam eget nisl tincidunt, varius odio a, tempus augue. Maecenas at erat ut diam luctus hendrerit. Nunc accumsan quam vitae mi commodo vehicula.
-                                    Quisque sagittis sagittis mauris, a faucibus libero aliquet sit amet. Proin accumsan quam sit amet velit ultrices auctor. Donec venenatis maximus velit ut fermentum. </p>
+                                  Klub można wynająć na wydarzenia takie jak: imprezy okolicznościowe, uroczystości obchodzone przez Organizatora, koncerty, szkolenia, konferencje, warsztaty tematyczne, wykłady lub inne tego typu spotkania.
+                                  Informujemy Was również, że osoba prywatna wyrażająca chęć wynajmu sali musi mieć powiązanie z Politechniką Wrocławską (posiadanie aktualnego statusu studenta, pracownika).
+                                  Koszt wynajmu jest uzależniony od terminu planowanego wydarzenia.
+                                  Zachęcamy do zapoznania się z regulaminem wynajmów oraz kontaktu z Nami.
+                                </p>
+                                <p>
+                                  Regulamin dostępny jest pod <a target="_blank" href="https://docs.google.com/document/d/1Kn7G-8tydq43uHcNM6DMBS3R__o7-RAxQUJTfRsE2Qk/edit?usp=sharing">tym linkiem</a>.
+                                </p>
                             </div>
                         </article>
                     </div>
@@ -22,7 +25,25 @@
                             <div class="mr-1 text-justify">
                                 <h5>Od czego rozpoczynamy wynajmy:</h5>
                                 <ul>
-                                    <li></li>
+                                    <li>minimum 5 tygodni przed planowanym wydarzeniem należy zgłosić się do przedstawicieli Klubu w celu dokonania rezerwacji</li>
+                                    <li>
+                                      minimum 30 dni przed planowanym wydarzeniem należy złożyć w Dziale BHP wniosek o wydarzenie uprzednio sporządzony z pomocą przedstawiciela Klubu
+                                    </li>
+                                    <li>
+                                      minimum 30 dni przed planowanym wydarzeniem należy złożyć wniosek o zgodę do Zarządu Klub Studenckiego Bajer, Kierownika Domu Studenckiego T-15 oraz Dyrektora Działu Domów Studenckich
+                                    </li>
+                                    <li>
+                                      minimum 20 dni przed planowanym wydarzeniem należy podpisać umowę wynajmu
+                                    </li>
+                                    <li>
+                                      nie później niż dwa tygodnie przed planowanym wydarzeniem należy dostarczyć szczegółowe informacje dotyczące wydarzenia, a także wytyczne odnośnie zapotrzebowań od strony technicznej
+                                    </li>
+                                    <li>
+                                      minimum 14 dni przed planowanym wydarzeniem należy dokonać opłaty za wynajem według uwag zawartych w umowie
+                                    </li>
+                                    <li>
+                                      minimum 7 dni przed planowanym wydarzeniem należy dostarczyć listę gości
+                                    </li>
                                 </ul>
                             </div>
                         </article>
@@ -59,24 +80,36 @@
       return {
         faqs: [
           {
-            question: '',
-            answer: ''
+            question: 'Ile maksymalnie osób może uczestniczyć w moim wydarzeniu?',
+            answer: 'W sali tanecznej Klubu Studenckiego Bajer zgodnie z zasadami BHP może uczestniczyć maksymalnie 100 osób.'
           },
           {
-            question: '',
-            answer: ''
+            question: 'Ile kosztuje wynajem sali?',
+            answer: 'Wydarzenie (imprezy okolicznościowe, uroczystości obchodzone przez Organizatora, koncerty) w piątek, sobotę w godzinach 19:00-01:00 - 400 złotych. Wydarzenie (imprezy okolicznościowe, uroczystości obchodzone przez Organizatora, koncerty) od poniedziałku do czwartku w godzinach 18:00-22:00 - 300 złotych. Wydarzenie (szkolenia, konferencje, warsztaty tematyczne, wykłady lub inne tego typu spotkania) - 50 złotych/godzinę.'
           },
           {
-            question: '',
-            answer: ''
+            question: 'Czy muszę być studentem Politechniki Wrocławskiej, żeby wynająć salę?',
+            answer: 'Według regulaminu osoba prywatna ubiegająca się o udostępnienie Sali Tanecznej KS Bajer musi mieć powiązanie z Politechniką Wrocławską (posiadanie aktualnego statusu studenta, pracownika).'
           },
           {
-            question: '',
-            answer: ''
+            question: 'Kiedy najpóźniej muszę zgłosić do Was chęć wynajęcia sali?',
+            answer: 'Rezerwacji należy dokonać co najmniej 5 tygodni przed planowaną datą wydarzenia.'
           },
           {
-            question: '',
-            answer: ''
+            question: 'Czy zostanie mi udostępniony sprzęt nagłośnieniowy i oświetleniowy znajdujący się w Klubie?',
+            answer: 'Tak, jeżeli wcześniej te kwestie zostaną omówione z przedstawicielem Klubu Studenckiego Bajer. '
+          },
+          {
+            question: 'Co leży w obowiązkach wynajmującego salę?',
+            answer: 'Osoba prywatna wyrażająca chęć wynajmu sali jest zobowiązana do wypełnienia wszystkich formalności zgodnie z zaleceniami przedstawiciela Klubu Studenckiego Bajer oraz przestrzegania regulaminu udostępniania oraz użytkowania sali tanecznej Klubu Studenckiego Bajer.'
+          },
+          {
+            question: 'Czy istnieje możliwość, żeby pojawiać się wcześniej w Klubie, żeby przygotować wszystko do wydarzenia?',
+            answer: 'Sala otwierana jest w godzinie rozpoczęcia Wynajmu określonej umową.'
+          },
+          {
+            question: 'Wydarzenia o jakim charakterze można zorganizować w Klubie?',
+            answer: 'Jest możliwość wynajęcia sali Klubu na wydarzenia takie jak: imprezy okolicznościowe, uroczystości obchodzone przez Organizatora, koncerty, szkolenia, konferencje, warsztaty tematyczne, wykłady lub inne tego typu spotkania.'
           }
         ]
       }
